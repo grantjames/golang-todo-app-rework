ae52f5356063f9bddc1c851436f9611a4a087e90
Grant <grantjames@live.co.uk>
2025-10-17 06:57:30 +0100

    Include cmd directories

---
+++ cmd/api/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"log/slog"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/google/uuid"
+	todos "grantjames.github.io/m/v2"
+)
+
+type traceIDKey struct{}
+
+var store = todos.NewStore("todos.json")
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/create/", handleCreate)
+	mux.HandleFunc("/get/", handleGet)
+	mux.HandleFunc("/update/", handleUpdate)
+	mux.HandleFunc("/delete/", handleDelete)
+	static := http.FileServer(http.Dir("../../web/static/about"))
+	mux.Handle("/about/", http.StripPrefix("/about/", static))
+
+	tmpl := template.Must(template.ParseFiles("../../web/templates/list.html"))
+	mux.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		items := store.List()
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		tmpl.Execute(w, items)
+	})
+
+	handler := traceIDMiddleware(mux)
+	slog.Info("API server listening on :5000")
+	log.Fatal(http.ListenAndServe(":5000", handler))
+}
+
+// contextLogger returns a slog.Logger with trace_id from context attached
+func contextLogger(ctx context.Context) *slog.Logger {
+	traceID, _ := ctx.Value(traceIDKey{}).(string)
+	return slog.With("trace_id", traceID)
+}
+
+func traceIDMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		traceID := uuid.New().String()
+		ctx := context.WithValue(r.Context(), traceIDKey{}, traceID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func handleCreate(w http.ResponseWriter, r *http.Request) {
+	logger := contextLogger(r.Context())
+	logger.Info("/create")
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var t todos.Todo
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	idx := store.Create(t.Description)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(idx))
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	logger := contextLogger(r.Context())
+	logger.Info("/get")
+
+	id := strings.TrimPrefix(r.URL.Path, "/get/")
+	todo, err := store.Get(id)
+	if err != nil {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	logger := contextLogger(r.Context())
+	logger.Info("/update")
+
+	var req struct {
+		Id     string `json:"id"`
+		Desc   string `json:"desc"`
+		Status string `json:"status"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	ok := store.Update(req.Id, req.Desc, req.Status)
+	if !ok {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	logger := contextLogger(r.Context())
+	logger.Info("/delete")
+
+	id := strings.TrimPrefix(r.URL.Path, "/delete/")
+	ok := store.Delete(id)
+	if !ok {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
---
+++ cmd/cli/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/google/uuid"
+	todos "grantjames.github.io/m/v2"
+)
+
+var todosFile = "todos.json"
+
+// This is not needed if we are just attaching the trace_id to the default logger
+//type traceIdKey struct{}
+
+func main() {
+	// Log to file so the console output is easier to read
+	f, err := os.Create("app.log") // This will truncate the file on each run
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// Generate a TraceID for this run using uuid
+	traceID := uuid.New().String()
+	// Does this need to be added to the context since we can just attach it to default logger?
+	// Would need to pass context around only if the CLI was a REPL that sent multiple requests to the store
+	//ctx := context.WithValue(context.Background(), traceIdKey{}, traceID)
+	logger := slog.New(slog.NewTextHandler(f, nil))
+	slog.SetDefault(logger.With("trace_id", traceID))
+
+	desc := flag.String("add", "", "Description of todo item to add")
+	updateId := flag.String("id", "", "ID of todo to update")
+	updateDesc := flag.String("desc", "", "New description for update")
+	updateStatus := flag.String("status", "", "New status for update")
+	deleteId := flag.String("delete", "", "ID of todo to delete")
+	flag.Parse()
+
+	store := todos.NewStore(todosFile)
+
+	if *desc != "" {
+		slog.Info("CLI calling store.Create()")
+		store.Create(*desc)
+	}
+
+	if *updateId != "" && (*updateDesc != "" || *updateStatus != "") {
+		slog.Info("CLI calling store.Update()")
+		ok := store.Update(*updateId, *updateDesc, *updateStatus)
+		if ok {
+			fmt.Printf("Updated todo #%s\n", *updateId)
+		}
+	}
+
+	if *deleteId != "" {
+		slog.Info("CLI calling store.Delete()")
+		ok := store.Delete(*deleteId)
+		if ok {
+			fmt.Printf("Deleted todo #%s\n", *deleteId)
+		}
+	}
+
+	fmt.Println("Todo list:")
+	slog.Info("CLI calling store.List()")
+	todos := store.List()
+	for i, t := range todos {
+		fmt.Printf("%s: %s [%s]\n", i, t.Description, t.Status)
+	}
+
+	// Wait for interrupt signal before saving and exiting
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Press Ctrl+C to exit.")
+	<-sigs
+}
